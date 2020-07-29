6f2e89221850ce5eaf14b7ddf8284c922d619d3c
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-29 14:15:58 +0200

    create todo Model

---
+++ src/main/java/com/mina/todolistjsp/model/Todo.java
@@ -0,0 +1,32 @@
+package com.mina.todolistjsp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+
+    @Size(min = 10, message = "Enter at least 10 characters")
+    private String description;
+
+    private Date targetDate;
+}
