a4d1544053e9689b5e341bda225cec6e920fb27f
Mina Rezkalla <minashakeralfy@gmail.com>
2020-07-29 11:18:47 +0200

    first commit

---
+++ src/main/java/com/mina/todolistjsp/TodoListJspApplication.java
@@ -0,0 +1,13 @@
+package com.mina.todolistjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mina/todolistjsp/TodoListJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mina.todolistjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
